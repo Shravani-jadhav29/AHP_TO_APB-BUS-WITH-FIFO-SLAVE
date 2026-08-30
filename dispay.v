c3c8bc8b9e04a6defd1989584b5810f114893a43
Shravani Shrikant Jadhav <2305029@ritindia.edu>
2026-08-30 12:53:21 +0530

    Create display_mux module for 7-segment display
    Implement display multiplexer module for 7-segment display.

---
+++ dispay.v
@@ -0,0 +1,76 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/17/2026 01:12:16 AM
+// Design Name: 
+// Module Name: DISPLAY
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+module display_mux(
+
+    input clk,
+    input [7:0] data,
+
+    output reg [3:0] an,
+    output [6:0] seg
+
+);
+
+reg [15:0] refresh_counter;
+reg refresh;
+reg [3:0] digit;
+
+always @(posedge clk)
+begin
+    refresh_counter <= refresh_counter + 1;
+
+    refresh <= refresh_counter[15];
+end
+
+always @(*)
+begin
+
+    case(refresh)
+
+        1'b0:
+        begin
+            an = 4'b1110;
+            digit = data[3:0];
+        end
+
+        1'b1:
+        begin
+            an = 4'b1101;
+            digit = data[7:4];
+        end
+
+    endcase
+
+end
+
+hex_to_7seg h1(
+
+    .hex(digit),
+    .seg(seg)
+
+);
+
+endmodule
